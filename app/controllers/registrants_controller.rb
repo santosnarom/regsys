fcd81eb28e8e031f0b5d59aee848e9835c38335e
santosnarom <santosnarom@gmail.com>
2016-08-07 01:10:23 +0800

    added export to csv

--- app/controllers/registrants_controller.rb
+++ app/controllers/registrants_controller.rb
@@ -5,6 +5,11 @@ class RegistrantsController < ApplicationController
   # GET /registrants.json
   def index
     @registrants = Registrant.all
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @registrants.as_csv }
+    end
   end
 
   # GET /registrants/1
--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -1,2 +1,12 @@
+require 'csv'
+
 class Registrant < ApplicationRecord
+  def self.as_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |item|
+        csv << item.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
